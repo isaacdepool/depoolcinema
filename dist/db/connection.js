770fc3517c7b6f3291822709e6d6fc14e4f2bfe4
IsaacDepool <isaacdepoolg@gmail.com>
2021-10-11 19:59:38 -0400

    prueba

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const db = new sequelize_1.Sequelize('cine', 'root', '', {
-    host: 'localhost',
+const db = new sequelize_1.Sequelize('bl7mvlsdyn0b6frwynql', 'unjorveeknqg9efg', 'Vv6qkgEyKMx8gvfZv0cK', {
+    host: 'bl7mvlsdyn0b6frwynql-mysql.services.clever-cloud.com',
     dialect: 'mysql',
 });
 exports.default = db;
